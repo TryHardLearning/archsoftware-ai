d22901cf054f7ebbd1a58999e68714cba9e88347
Lucas C. Bresolino <lucascaladob@gmail.com>
2024-12-11 00:12:24 -0300

    assets

---
+++ src/assets/Data.js
@@ -0,0 +1,134 @@
+const AllMyProjects = [
+    {
+      Link: "https://github.com/TryHardLearning/waycicles-ts",
+      Name: "e-commerce Web application",
+      Description: "Website Developeded using React.js and Typescript",
+    },
+    {
+      Link: "https://github.com/TryHardLearning/on-golang",
+      Name: "Golang CRUD with Gorilla Mux and MongoDB",
+      Description: "MVC Applicatiion",
+    },
+    {
+      Link: "https://github.com/TryHardLearning/finance-ai",
+      Name: "AI for Finannce developed in Bootcamp",
+      Description: "ERP with GPT 3.5",
+    },
+    {
+      Link: "https://github.com/TryHardLearning/shopping-app",
+      Name: "e-commerce Web Application",
+      Description: "React.js Javascript @Styled-Components",
+    },
+    {
+      Link: "https://github.com/TryHardLearning/NFS-App",
+      Name: "ERP Web Application",
+      Description: "React.js Javascript @Nivo @Styled-Componnets",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Distributed-Applications",
+        Name: "Distributed and Concurrent Applications",
+        Description: "Java Python C and Go with gRPC",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Fitness-App",
+        Name: "Fitness app Mobile",
+        Description: "Dart Flutter",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Desktop-CSharp",
+        Name: "Descktop Application",
+        Description: "C# .Net",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/DeepWoods",
+        Name: "2D Action RPG Game",
+        Description: "C# .Net Godot",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Pos-Desenvolvimento-Mobile",
+        Name: "Game Class Projects",
+        Description: "C# .Net Godot",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/mYServer",
+        Name: "Server Application for ERP",
+        Description: "Java Spring Boot Spring Web Hibernate",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Web-Application/",
+        Name: "Server Application MVC",
+        Description: "Java Spring Boot Spring Web Hibernate",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/LifeCycle",
+        Name: "Web e-commerce",
+        Description: "HTML CSS e Bootstrap",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/react-app/",
+        Name: "First E-coomerce project",
+        Description: "React.js Javascript @Styled-Components",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/websocket",
+        Name: "Java Web Socket Communication",
+        Description: "Java WebSocket and Threads",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/AD",
+        Name: "Java data transfer",
+        Description: "Java",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/WayCircle-Final",
+        Name: "e-commerce backend",
+        Description: "Java Spring Boot",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/On",
+        Name: "Object Orientation 2",
+        Description: "Java Spring Boot",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/On",
+        Name: "Object Orientation 2",
+        Description: "Java Spring Boot",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Front-end-Project",
+        Name: "Webpages",
+        Description: "HTML and CSS",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/PostgreSQL",
+        Name: "DataWareHouse",
+        Description: "ETL Data in PostgreSQL",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/Upload-Image",
+        Name: "HTML DOM manipulation",
+        Description: "Javascript HTML and CSS",
+    },
+
+    {
+        Link: "https://github.com/TryHardLearning/PortifolioEn",
+        Name: "First portifolio website",
+        Description: "HTML and CSS",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/University",
+        Name: "Fake University Website",
+        Description: "HTML and CSS",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/WebMedicalSite",
+        Name: "Fake Medical Website",
+        Description: "HTML and CSS",
+    },
+    {
+        Link: "https://github.com/TryHardLearning/C-Alg",
+        Name: "Algorithms and Data Structures",
+        Description: "Only C Language",
+    },
+  ];
+export default AllMyProjects;
