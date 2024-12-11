ba520cc1edf34f9649cfa8f71e8bbadc353f21b3
Lucas C. Bresolino <lucascaladob@gmail.com>
2024-12-11 00:10:33 -0300

    feat: landpage

---
+++ src/pages/LandPage.jsx
@@ -0,0 +1,18 @@
+import { Banner, ContactUs, Footer, Header, OurTeam, Projects, Services, AboutUs, WorkWIth} from "../page-components"
+
+export default function LandPage() {
+
+  return (
+    <>
+     <Header/>
+      <Banner/>
+      <AboutUs/>
+      <Services/>
+      <Projects/>
+      <OurTeam/>
+      <WorkWIth/>
+      <ContactUs/>
+      <Footer/>
+    </>
+  )
+}
