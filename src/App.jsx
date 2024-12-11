c6b6f50a85d445e1097759d345e6e425e604a2e2
Lucas C. Bresolino <lucascaladob@gmail.com>
2024-12-11 00:13:24 -0300

    main files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { LandPage } from './pages'
+
+function App() {
+
+  return (
+    <>
+      <LandPage/>
+    </>
+  )
+}
+
+export default App
