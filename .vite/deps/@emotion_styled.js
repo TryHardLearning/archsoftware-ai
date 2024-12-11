1e8a749c3f3f78909588f591fa81a40fb34f4c18
Lucas C. Bresolino <lucascaladob@gmail.com>
2024-12-11 00:58:16 -0300

    system files

---
+++ .vite/deps/@emotion_styled.js
@@ -0,0 +1,10 @@
+import {
+  init_emotion_styled_browser_development_esm,
+  newStyled
+} from "./chunk-2ZI3Y6CG.js";
+import "./chunk-C6XSHW46.js";
+import "./chunk-45FXRYJS.js";
+init_emotion_styled_browser_development_esm();
+export {
+  newStyled as default
+};
